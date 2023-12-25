404f4c54858b04b7778c23288977993c80a02f34
rickiey <rickiey@qq.com>
2023-12-25 16:49:41 +0800

    update axum

--- axum_svc/src/main.rs
+++ axum_svc/src/main.rs
@@ -6,11 +6,14 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "{\"Hello\": \"World!\"}" }));
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
